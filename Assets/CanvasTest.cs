1fb7ff9dc6a241083e015fa43450917cdf04d622
William Burke <wwm0nkey@outlook.com>
2016-07-27 21:45:37 -0400

    First Test

---
+++ Assets/CanvasTest.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CanvasTest : MonoBehaviour {
+
+    public Transform childObj;
+	// Use this for initialization
+	void Start () {
+        childObj = transform.GetChild(0);
+        childObj.gameObject.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/RNG.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class RNG : MonoBehaviour {
+    public int randomNum;
+    public int correctNum = 24;
+    public int numRange;
+    public bool ispaused = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       // Debug.Log(SceneManager.GetActiveScene().name);
+       if(Input.GetKey(KeyCode.W))
+        {
+            randomNum = Random.Range(0, numRange);
+            if (randomNum == correctNum && SceneManager.GetActiveScene().name == "Test_1")
+            {
+                SceneManager.LoadScene("Enemy_Test");
+            }
+        }
+
+
+
+        if (Input.GetKeyDown(KeyCode.Escape) && SceneManager.GetActiveScene().name == "Enemy_Test")
+        {
+            if (ispaused == false)
+            {
+                Time.timeScale = 0.0f;
+                GameObject.Find("Canvas").GetComponent<CanvasTest>().childObj.gameObject.SetActive(true);
+                ispaused = true;
+            }
+            else if(ispaused == true)
+            {
+                SetTrueFalse();
+            }
+        }
+	}
+
+    public void SetTrueFalse()
+    {
+        Time.timeScale = 1.0f;
+        GameObject.Find("Canvas").GetComponent<CanvasTest>().childObj.gameObject.SetActive(false);
+        ispaused = false;
+    }
+}
